697f55415b97d5b1608a7d07f3c0bbb0768b7a94
thonggit <dobathongalasan@gmail.com>
2024-11-15 10:20:51 +0700

    day 1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import Navigation from './customer/components/Navigation/Navigation';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+    <Navigation/>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
